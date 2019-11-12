a41a266f5a7f0674c38562c8d46d747baf3156f1
Matt Welson <matthew.welson@gmail.com>
2019-11-12 20:39:30 +1300

    feat(base): init

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,6 +2,6 @@
 import 'typeface-montserrat'
 import 'typeface-merriweather'
 
-import { wrapRootElement as wrap } from './wrap-root-element'
+import { wrapRootElement as wrap } from './src/helpers/wrap-root-element'
 
 export const wrapRootElement = wrap
